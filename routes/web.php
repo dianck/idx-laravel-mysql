6a86380347b5e9744a2427d7027938ffbe430ef8
dianck2002 <dianck2002@gmail.com>
2024-09-17 04:38:54 +0000

    first

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:196193281.
+Route::get('/users', function () {
+    // return view('about');
+    foreach(User::all() as $user){
+        dump($user->name);
+        dump($user->email);
+    }
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+
+});
